cdb8105a17650d51267980cafb777d0df362fee3
jysim3 <junyang.0607@gmail.com>
2019-07-27 16:16:15 +1000

    login form, store front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,100 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Layout, Menu, Breadcrumb, List, Avatar, Icon, Modal, Form, Input, Checkbox } from 'antd';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import LoginForm from "./LoginForm.js";
+import Store  from "./Store.js";
+import Sell  from "./Sell.js";
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+
+
+class App extends React.Component {
+    state = {
+        login: false
+    };
+  
+    MenuOnClick = ({item, keyPath}) => {
+        if (keyPath[0] === "login") {
+            console.log("jsdafjkl");
+            this.setState({
+                login: true
+            })
+        }
+
+    }
+    handleOk = e => {
+        console.log(e);
+        this.setState({
+            login: false
+        })
+    }
+
+    render() {
+        const MainMenu = () => (
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="horizontal"
+                onClick={this.MenuOnClick}
+                style={{ lineHeight: '64px' }}>
+                <Menu.Item key="1"
+                    //style={{ float: 'left' }}
+                    >
+                    <Icon type="shopping" />
+                    <span>Buy</span>
+                </Menu.Item>
+                <Menu.Item key="2"
+                    //style={{ float: 'left' }}
+                    >
+                    <Icon type="dollar" />
+                    <span>Sell / Donate</span>
+                </Menu.Item>
+                <Menu.Item
+                    //style={{ float: 'left' }}
+                    key="9">
+                    <Icon type="shopping-cart" />
+                    <span>Cart</span>
+                </Menu.Item>
+                <Menu.Item
+                    //style={{ float: 'left' }}
+                    key="login">
+                        <span>
+                            <Icon type="user" />
+                            <span>Login</span>
+                        </span>
+                    
+    
+                </Menu.Item>
+                <Modal
+          title="Login form"
+          visible={this.state.login}
+          onOk={this.handleOk}
+          onCancel={() => {this.setState({login: false})}}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <LoginForm></LoginForm>
+      </Modal>
+            </Menu>
+        );
+        return (
+            <Router>
+                <Layout style={{ minHeight: '100vh' }}>
+
+                    <Header  >
+                        <div className="logo" />
+                        <MainMenu />
+                    </Header>
+                    <Layout>
+
+                        <Route path="/store" component={Store} />
+
+                        <Footer style={{ textAlign: 'center' }}>Lmao footer </Footer>
+                    </Layout>
+
+                </Layout>
+            </Router>
+        );
+    }
 }
 
 export default App;
---
+++ frontend/src/Sell.js
@@ -0,0 +1,10 @@
+import React from 'react';
+class Sell extends React.Component {
+
+
+
+    render() {
+        return (<div>Steven lieks to eAt nUgGeTs</div>);
+    }
+}
+export default Sell;
